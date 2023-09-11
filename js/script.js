21f3858ca6f994c82cb9fc8ba8589441b37cc7e4
Ridho Ismail <ridhoismailn@gmail.com>
2023-09-11 19:45:25 +0700

    menambahkan fungsi di js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function switchPersegi() {
+    let p = document.getElementById("info");
+    if (p.style.display === "none") {
+        p.style.display = "block";
+    } else {
+        p.style.display = "none";
+    }
+}
